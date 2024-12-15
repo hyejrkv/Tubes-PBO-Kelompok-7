9e649a20c83a798827df4f87abe7ec6e22680726
Nurul Izzah <147795093+hyejrkv@users.noreply.github.com>
2024-12-15 16:32:17 +0700

    Add files via upload

---
+++ dealermobil/src/main/java/com/tubespbo/dealermobil/DealermobilApplication.java
@@ -0,0 +1,13 @@
+package com.tubespbo.dealermobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealermobilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealermobilApplication.class, args);
+	}
+
+}
---
+++ dealermobil/src/test/java/com/tubespbo/dealermobil/DealermobilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubespbo.dealermobil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealermobilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
